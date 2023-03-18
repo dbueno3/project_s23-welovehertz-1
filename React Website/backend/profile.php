0c03bec5ac0a18819e5ac8e59effd734b1129738
MathewDebug <mathewyeung123@gmail.com>
2023-03-18 15:25:28 -0400

    Updating files

---
+++ React Website/backend/profile.php
@@ -0,0 +1,29 @@
+<<<<<<< Updated upstream
+=======
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: *");
+
+include 'config.php';
+$db = new DbConn;
+$conn = $db->connect();
+
+$method = $_SERVER['REQUEST_METHOD'];
+switch($method) {
+    case "GET":
+        $select_user = "SELECT * FROM users";
+        $data = array();
+        while($row = mysqli_fetch_array($result)) {
+            array_push($data,$row['first_name']."".$row['last_name']."".$row['email']);
+        }
+
+        if($param->execute()) {
+            $response = ['status' => 1, 'message' => 'Record Created'];
+        }else {
+            $response = ['status' => 0, 'message' => 'Record Failed to Create'];
+        }
+        echo json_encode($response);
+        break;
+}
+?>
+>>>>>>> Stashed changes

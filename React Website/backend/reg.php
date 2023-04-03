dd7ae5761fe07d04a0e1bc2c28c3d8892b0aa9be
ksy990719 <ksy990719@hotmail.com>
2023-04-03 18:14:32 -0400

    optimized and updated registration page #35
    adding validation for each input field

--- React Website/backend/reg.php
+++ React Website/backend/reg.php
@@ -7,23 +7,47 @@ $db = new DbConn;
 $conn = $db->connect();
 
 $method = $_SERVER['REQUEST_METHOD'];
-switch($method) {
-    case "POST":
-      $user = json_decode(file_get_contents('php://input'));
-      $hashed_pass = password_hash($user->password,PASSWORD_DEFAULT);
+switch ($method) {
+  case "POST":
+    $user = json_decode(file_get_contents('php://input'));
+    echo $user->first_name; 
+    // Validate input fields
+    $errors = [];
+    if (!preg_match('/^[a-zA-Z ]+$/', $user->first_name)) {
+      $errors['first_name'] = 'First name should only contain letters and spaces';
+    }
+    if (!preg_match('/^[a-zA-Z ]+$/', $user->last_name)) {
+      $errors['last_name'] = 'Last name should only contain letters and spaces';
+    }
+    if (!filter_var($user->email, FILTER_VALIDATE_EMAIL)) {
+      $errors['email'] = 'Invalid email address';
+    }
+    if (strlen($user->password) < 8) {
+      $errors['password'] = 'Password should be at least 8 characters long';
+    }
+    if ($user->password !== $user->confirmPassword) {
+      $errors['confirm_password'] = 'Passwords do not match';
+    }
+
+    if (count($errors) > 0) {
+      $response = ['status' => 0, 'message' => 'Input validation failed', 'errors' => $errors];
+      echo json_encode($response);
+      exit();
+    } else {
+      $hashed_pass = password_hash($user->password, PASSWORD_DEFAULT);
       $sql = "INSERT INTO users (first_name, last_name, email, password) VALUES (:first, :last, :email, :password)";
       $param = $conn->prepare($sql);
       $param->bindParam(':first', $user->first_name);
       $param->bindParam(':last', $user->last_name);
       $param->bindParam(':email', $user->email);
       $param->bindParam(':password', $hashed_pass);
-      if($param->execute()) {
+      if (count($errors) == 0 && $param->execute()) {
         $response = ['status' => 1, 'message' => 'Record Created'];
-      }else {
+      } else {
         $response = ['status' => 0, 'message' => 'Record Failed to Create'];
       }
       echo json_encode($response);
       break;
-}
-
-?>;
+    }
+} 
+?>
--- React Website/src/pages/RegistrationPage.jsx
+++ React Website/src/pages/RegistrationPage.jsx
@@ -1,10 +1,10 @@
 import '../styles/RegistrationPage.css';
 import React, { useState } from "react";
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import Axios from 'axios';
 //Registration Page
 
-export default function Register () {
+export default function Register() {
 
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
@@ -12,52 +12,100 @@ export default function Register () {
     const [last_name, setLastName] = useState('');
     const [confirmPassword, setConfirmPassword] = useState("");
     const [success, setSuccess] = useState(false);
-    const [error, setError] = useState(null);
+    const [errors, setErrors] = useState({
+        first_name: '',
+        last_name: '',
+        email: '',
+        password: '',
+        confirmPassword: ''
+    });
 
-    const data  = {
-        first_name:first_name,
-        last_name:last_name,
-        email:email,
-        password:password,
+    const data = {
+        first_name: first_name,
+        last_name: last_name,
+        email: email,
+        password: password,
+        confirmPassword: confirmPassword,
     }
-
+    const nameRegex = /^[a-zA-Z]+$/;
     const handleSubmission = (event) => {
         //This prevents the page from reloading and losing our current state
         event.preventDefault();
+
+        const newErrors = {
+            first_name: '',
+            last_name: '',
+            email: '',
+            password: '',
+            confirmPassword: ''
+        };
+
+        if (password.length < 8) {
+            newErrors.password = 'Password should be at least 8 characters long';
+        }
+
+        if (!nameRegex.test(first_name.trim())) {
+            newErrors.first_name = 'First name can only contain letters';
+        }
+
+        if (!nameRegex.test(last_name.trim())) {
+            newErrors.last_name = 'Last name can only contain letters';
+        }
+
+        if (confirmPassword!=password) {
+            newErrors.confirmPassword = 'confirm password must match password';
+        }
+
+        if (Object.values(newErrors).some(error => error !== '')) {
+            setErrors(newErrors);
+            setSuccess(false);
+            return;
+        }
+
         console.log(data)
         Axios.post('https://www-student.cse.buffalo.edu/CSE442-542/2023-Spring/cse-442h/backend/reg.php', data)
-          .then(response => {
-            setSuccess(true);
-            setError(null);
-          })
-          .catch(error => {
-            setError(error.message);
-            setSuccess(false);
-          });
-    }  
+            .then(response => {
+                setSuccess(true);
+                setErrors({
+                    first_name: '',
+                    last_name: '',
+                    email: '',
+                    password: '',
+                    confirmPassword: ''
+                });
+            })
+            .catch(error => {
+                setErrors({ ...errors, email: error.message });
+                setSuccess(false);
+            });
+    }
 
     return (
         <>
             <div className='register'>
                 <div className="register-form">
                     <form className="reg-inputs" onSubmit={handleSubmission}>
-                        <label for="name">First Name</label>
-                        <input value={first_name} onChange={(event) => setFirstName(event.target.value)} type="name" placeholder='First Name' name='name'required/>
-                        <label for="name">Last Name</label>
-                        <input value={last_name} onChange={(event) => setLastName(event.target.value)} type="name" placeholder='Last Name' name='name'required/>
-                        <label for="email">Email</label>
-                        <input value={email} onChange={(event) => setEmail(event.target.value)} type="email" placeholder="Email" name="email" required/>
-                        <label for="password">Password</label>
-                        <input value={password} onChange={(event) => setPassword(event.target.value)} type="password" placeholder="Password" name="password" required/>
-                        <label for="password">Confirm Password</label>
-                        <input value={confirmPassword} onChange={(event) => setConfirmPassword(event.target.value)} type="password" placeholder="Confirm Password" name="password" required/>
+                        <label htmlFor="first_name">First Name</label>{errors.first_name && <div className="error-message">{errors.first_name}</div>}
+                        <input value={first_name} onChange={(event) => setFirstName(event.target.value)} type="name" placeholder='First Name' name='first_name' required />
+                        <label htmlFor="last_name">Last Name</label>
+                        <input value={last_name} onChange={(event) => setLastName(event.target.value)} type="name" placeholder='Last Name' name='last_name' required />
+                        {errors.last_name && <div className="error-message">{errors.last_name}</div>}
+                        <label htmlFor="email">Email</label>
+                        <input value={email} onChange={(event) => setEmail(event.target.value)} type="email" placeholder="Email" name="email" required />
+                        {errors.email && <div className="error-message">{errors.email}</div>}
+                        <label htmlFor="password">Password</label>
+                        <input value={password} onChange={(event) => setPassword(event.target.value)} type="password" placeholder="Password" name="password" required />
+                        {errors.password && <div className="error-message">{errors.password}</div>}
+                        <label htmlFor="confirmPassword">Confirm Password</label>
+                        <input value={confirmPassword} onChange={(event) => setConfirmPassword(event.target.value)} type="password" placeholder="Confirm Password" name="confirmPassword" required />
+                        {errors.confirmPassword && <div className="error-message">{errors.confirmPassword}</div>}
                         <button>Register</button>
-                        {error && <div>{error}</div>}
+                        {errors.email && <div className="error-message">{errors.email}</div>}
                         {success && <div>Account Creation Success!</div>}
                     </form>
                     <button className="create-account-button" ><Link to="https://www-student.cse.buffalo.edu/CSE442-542/2023-Spring/cse-442h/login">Have An Account? Click Here</Link></button>
                 </div>
-            </div>  
-        </>  
+            </div>
+        </>
     );
 }

7c872a03ad25fbdb4690bc099b14a7cfdcc58fd2
MathewDebug <mathewyeung123@gmail.com>
2023-04-09 04:36:20 -0400

    Merging Update #79

--- React Website/backend/reg.php
+++ React Website/backend/reg.php
@@ -7,23 +7,53 @@ $db = new DbConn;
 $conn = $db->connect();
 
 $method = $_SERVER['REQUEST_METHOD'];
-switch($method) {
-    case "POST":
-      $user = json_decode(file_get_contents('php://input'));
-      $hashed_pass = password_hash($user->password,PASSWORD_DEFAULT);
-      $sql = "INSERT INTO users (first_name, last_name, email, password) VALUES (:first, :last, :email, :password)";
+switch ($method) {
+  case "POST":
+    $user = json_decode(file_get_contents('php://input'));
+    $sql = "SELECT COUNT(*) FROM users WHERE email = :email";
+    $stmt = $conn->prepare($sql);
+    $stmt->bindParam(':email', $user->email);
+    $stmt->execute();
+    $count = $stmt->fetchColumn();
+    // Validate input fields
+    $errors = [];
+
+    if ($count > 0) {
+      $errors['email'] = 'Email address already exists';
+    }
+    if (!preg_match('/^[a-zA-Z ]+$/', $user->first_name)) {
+      $errors['first_name'] = 'First name should only contain letters and spaces';
+    }
+    if (!preg_match('/^[a-zA-Z ]+$/', $user->last_name)) {
+      $errors['last_name'] = 'Last name should only contain letters and spaces';
+    }
+    if (!filter_var($user->email, FILTER_VALIDATE_EMAIL)) {
+      $errors['email'] = 'Invalid email address';
+    }
+    if ($user->password !== $user->confirm_password) {
+      $errors['confirm_password'][] = 'Passwords do not match';
+    }
+    if (strlen($user->password) < 8) {
+      $errors['password'][] = 'Password should be at least 8 characters long';
+    }
+    if (count($errors) > 0) {
+      $response = ['status' => 0, 'message' => 'Input validation failed', 'errors' => $errors];
+      echo json_encode($response);
+      exit();
+    } else {
+      $sql = "INSERT INTO users (first_name, last_name, email, password, favorite_list) VALUES (:first, :last, :email, :password, 1)";
       $param = $conn->prepare($sql);
       $param->bindParam(':first', $user->first_name);
       $param->bindParam(':last', $user->last_name);
       $param->bindParam(':email', $user->email);
-      $param->bindParam(':password', $hashed_pass);
-      if($param->execute()) {
+      $hashed_password = password_hash($user->password, PASSWORD_DEFAULT);
+      $param->bindParam(':password', $hashed_password);
+      if ($param->execute()) {
         $response = ['status' => 1, 'message' => 'Record Created'];
-      }else {
+      } else {
         $response = ['status' => 0, 'message' => 'Record Failed to Create'];
       }
       echo json_encode($response);
       break;
+    }
 }
-
-?>;

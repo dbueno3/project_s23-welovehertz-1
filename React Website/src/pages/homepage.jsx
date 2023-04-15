630fcd65c3ad843bd8b2c0b8c455c83de1655c98
ksy990719 <ksy990719@hotmail.com>
2023-04-14 20:08:36 -0400

    Updated dynamic listing cards (unfinish) #91

--- React Website/src/pages/homepage.jsx
+++ React Website/src/pages/homepage.jsx
@@ -1,45 +1,46 @@
-import { Link, useNavigate } from 'react-router-dom'
-import BackGround from '../pictures/background.jpg'
+import { Link, useNavigate } from 'react-router-dom';
+import BackGround from '../pictures/background.jpg';
+import { useState, useEffect } from 'react';
+import Axios from 'axios';
 import '../styles/homepage.css'
 import * as React from 'react';
 import ResidentPage from './residential-page';
 
-/* Images */
-import porter from '../pictures/Porter.jpg';
+import porter from '../pictures/porter.jpg';
 import wilkenson from '../pictures/wilkeson.jpg';
 import gov from '../pictures/governors.jpg';
 import fargo from '../pictures/fargo.jpg';
 import richmond from '../pictures/richmond.jpg';
 import redjacket from '../pictures/redjacket3.jpg';
-import southlake from '../pictures/southlake.jpg';
+import south from '../pictures/south.jpg';
 import hadley from '../pictures/hadley.jpg'
 import spaulding from '../pictures/spaulding.jpg'
 import creekside from '../pictures/creekside.jpg'
 
 
+const Homepage = () => {
+    const [listings, setListings] = useState([{
+        id:'',
+        residence:''
+    }]);
 
-const listings = [
-    
-    { id: 1, name: 'Fargo', image: fargo },
-    { id: 2, name: 'Richmond', image: richmond },
-    { id: 3, name: 'Red Jacket', image: redjacket },
-    { id: 4, name: 'Porter Dorm', image: porter },
-    { id: 5, name: 'Wilkenson', image: wilkenson },
-    { id: 6, name: 'South Lake', image: southlake },
-    { id: 7, name: 'Hadley', image: hadley },
-    { id: 8, name: 'Spaulding', image: spaulding },
-    { id: 9, name: 'Governors', image: gov },
-    { id: 10, name: 'Creekside East', image: creekside },
-    { id: 11, name: 'Creekside West', image: creekside }
-];
+    useEffect(() => {
+        Axios.get('https://www-student.cse.buffalo.edu/CSE442-542/2023-Spring/cse-442h/backend/listingcard.php', {
+        })
+            .then(response => {
+                const response_json = JSON.parse(response.data.substring(1));
+                console.log(response_json)
+                setListings(response_json)
+            })
+            .catch(error => console.error(error));
+    }, []);
 
-export default function Homepage() {
     const navigate = useNavigate();
 
     const handleListingClick = (id) => {
-        if (0 < id < 12){
+        if (0 < id < 12) {
             navigate(`/CSE442-542/2023-Spring/cse-442h/${id}`)
-        }else{
+        } else {
             navigate(`/CSE442-542/2023-Spring/cse-442h/`)
         }
     }
@@ -47,20 +48,21 @@ export default function Homepage() {
     return (
         <>
             <div className='home' style={{ backgroundImage: `url(${BackGround})` }}>
-                <div className='headerContainer' >
+                <div className='headerContainer'>
                     <h1>HOUSE FINDER</h1>
-                    <p> Easily Figure Out Where To Live</p>
+                    <p>Easily Figure Out Where To Live</p>
                 </div>
-
             </div>
             <div className='listings'>
                 {listings.map(listing => (
                     <div key={listing.id} className='listing-card' onClick={() => handleListingClick(listing.id)}>
-                        <img src={listing.image} alt={listing.name} />
-                        <h2>{listing.name}</h2>
+                        <img src={"../pictures/" + listing.residence.split(" ")[0].toLowerCase() + ".jpg"} alt={listing.residence.split(" ")[0].toLowerCase()} />
+                        <h2>{listing.residence}</h2>
                     </div>
                 ))}
             </div>
         </>
-    )
-}
+    );
+};
+
+export default Homepage;
